3947952c4c36759b931bb60939732c5fbfeefe63
xYzDl <39340202+z475811790@users.noreply.github.com>
2018-09-27 19:35:04 +0800

    add user entity

--- XServer/src/com/util/EntityUtil.java
+++ XServer/src/com/util/EntityUtil.java
@@ -8,6 +8,12 @@ import com.entity.Student;
  * @description 表实体处理相关工具
  */
 public class EntityUtil {
+	/**
+	 * 根据实体对象获取其Id值
+	 * 
+	 * @param entity
+	 * @return
+	 */
 	public static Integer getId(Object entity) {
 		if (entity instanceof Student)
 			return ((Student) entity).getId();
